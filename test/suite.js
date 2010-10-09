d83967239f814e22b9a1025d8bc99f9833235c97
Paul Elliott <paul@hashrocket.com>
2010-10-09 09:36:51 -0400

    Test that it applies the default value on blur when no value is present

--- test/suite.js
+++ test/suite.js
@@ -25,4 +25,11 @@ $(function() {
     $("#test_input").focus();
     equals($("#test_input").val(), "Corbin Dallas");
   });
+
+  test("it applies the default value on blur when no value is present", function() {
+    $("#test_input").default_value("Name").val("");
+    $("#test_input").blur();
+    equals($("#test_input").val(), "Name");
+  });
+
 });
